b9c60b720a24418b43ecd8c201ad9434def71d36
ErlangSurya <144051278+ErlangSurya@users.noreply.github.com>
2024-02-23 16:40:17 -0800

    numerical sequence and fix various size issues on mobile

--- scripts/numerical-sequence-script.js
+++ scripts/numerical-sequence-script.js
@@ -1,24 +1,24 @@
 const questionContainer = document.querySelector('#question');
 const buttons = document.querySelectorAll('.key');
-const displayscore = document.querySelector('#score');
 const restartButtons = document.querySelectorAll('.restartButton');
-const time = document.querySelector('#timer')
-const duration = 30*1000;
 const gameOverMessage = document.querySelector('#gameOverMessage')
 const timerBar = document.querySelector('#timer-bar')
 
 let question;
-let score = 0;
-let remainingTime = duration/1000;
-let length = score + 4;
 let answer;
-
+let game_length;
 let intervalID;
 let timeoutID;
 
 
 
 
+function restart(){
+  document.getElementById('gameOverScreen').style.display = 'none';
+  document.getElementById('startScreen').style.display = 'none';
+  game_length = 8;
+  memory_phase();
+}
 
 
 
@@ -26,31 +26,50 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min); 
 }
 
+function answer_display(answer, game_length){
+  questionContainer.innerHTML = '';
+  let i = answer.length
+  for (let j = 0 ; j<game_length; j++){
+    if (j<i){
+      const numberSpan = document.createElement('span');
+      numberSpan.textContent = answer[j];
+      questionContainer.appendChild(numberSpan);
+    }
+    else{
+      const numberSpan = document.createElement('span');
+      numberSpan.textContent = question[j];
+      numberSpan.style.visibility = 'hidden';
+      questionContainer.appendChild(numberSpan);
+    }
+  }
+}
+
 
 function answering_phase(){
     buttons.forEach(button => {button.disabled =false;});
+    answer = [];
+    answer_display(answer,game_length);
 }
 
 function memory_phase(){
     buttons.forEach(button => {button.disabled =true;});
-    // run the timer
     let width=100;
+    // fixed 10 seconds timer, irrespective of level
     intervalID = setInterval(function(){
-        console.log('shrinking');
-        width -= 10 / length;
+        width -= 1;
         timerBar.style.width = width +'%';
         if (width<=0){
             clearInterval(intervalID);
-            answering_phase()
+            setTimeout(answering_phase, 500); // because transition has 0.5s delay
         }
     }, 100)
-    randomQuestion();
+    question = randomQuestion();
 }
 
 function randomQuestion(){
     questionContainer.innerHTML='';
     question = [];
-    for (let i = 0; i<length; i++){
+    for (let i = 0; i<game_length; i++){
         question.push(getRandomInt(0,10));
     }
     for (const num of question){
@@ -68,10 +87,6 @@ function lose(){
     clearInterval(intervalID);
 
 }
-function lose_time(){
-  time.textContent = `Time: 00`;
-  lose();
-}
 
 function win(){
   gameOverMessage.textContent = 'You win!';
@@ -80,46 +95,43 @@ function win(){
   clearInterval(intervalID);
 }
 
-function pad(x){
-  if (x>=10){
-    return x.toString();
+
+
+
+
+
+function arrayEqual(arr1, arr2){
+  if (arr1.length !== arr2.length){
+    return false 
   }
-  else{
-    return '0'+x.toString();
+  for (let i=0; i<arr1.length; i++){
+    if (arr1[i]!==arr2[i]){
+      return false
+    }
   }
+  return true
 }
-
-function restart(){
-    document.getElementById('gameOverScreen').style.display = 'none';
-    document.getElementById('startScreen').style.display = 'none';
-    score = 0 
-    memory_phase()
-    buttons.forEach(button => {button.style.backgroundColor ='white';})
-    buttons.forEach(button => {button.disabled =false;})   
-}
-
-
-function updateCountdown(){
-  remainingTime -= 1;
-  time.textContent = `Time: ${pad(remainingTime)}`;
-}
-
-
 
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
       const pressedNumber = Number(button.textContent);
-      if (pressedNumber===question){
-        score+=1
-        question = randomQuestion()
-        if (score===30){
-          win()
+      answer.push(pressedNumber);
+      answer_display(answer, game_length);
+      if (answer.length === game_length){
+        //check answer
+        if (arrayEqual(answer, question)){
+          if (answer.length === 16){
+            win();
+          }
+          else{
+          game_length += 1 ;
+          memory_phase();
+          }
         }
-      }
-      else{
-        button.style.backgroundColor ='red';
-        lose()
+        else{
+          lose();
+        }
       }
     });
   });
