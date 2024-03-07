a83508ec3982fce0b5f03280a206d2ecc5281fcd
ErlangSurya <144051278+ErlangSurya@users.noreply.github.com>
2024-03-06 19:15:22 -0800

    [feat] swaps blind, timers, styles

--- scripts/swaps.js
+++ scripts/swaps.js
@@ -1,32 +1,33 @@
 const startButton = document.querySelector('#startButton');
 const restartButtons = document.querySelectorAll('.restartButton');
 const grid = document.querySelector('#grid');
+const stopwatch = document.querySelector('#stopwatch');
+const memo =  document.querySelector('#memo');
+const execute =  document.querySelector('#execute');
+const gameplayScreen = document.querySelector('#gameplayScreen');
 
 let N;
 let chosen =[];
+let intervalStopwatch;
+let intervalMemo;
+let intervalExecute;
 
 
 
 
 startButton.addEventListener('click',() =>{
     let N_dropdown = document.getElementById("N");
-    N = Number(N_dropdown.options[N_dropdown.selectedIndex].value);
-    let question = random_perm(N*N);
-    for (let i = 0; i<N*N; i++){
-        const newCell = document.createElement('button');
-        newCell.classList.add('cell');
-        newCell.textContent = question[i];
-        grid.appendChild(newCell);
-    }
-    grid.style.gridTemplateColumns = `repeat(${N}, ${'10vw'})`;
+    N = Number(N_dropdown.options[N_dropdown.selectedIndex].value); 
 }
 )
 
 
 function update(){
     check_chosen();
-    window.requestAnimationFrame(update)
+    window.requestAnimationFrame(update);
 }
+window.requestAnimationFrame(update);
+
 
 function check_chosen(){
     if (chosen.length==2){
@@ -55,24 +56,73 @@ function game_over(){
     const cells = document.querySelectorAll('.cell');
     cells.forEach(cell =>{
         cell.disabled = true;
+        cells.forEach(cell =>{cell.style.textIndent = '0px'; })
     })
+    clearInterval(intervalStopwatch);
+    clearInterval(intervalExecute);
+    const resetButton = document.createElement('button');
+    resetButton.textContent = 'Reset';
+    resetButton.classList.add('restartButton');
+    gameplayScreen.appendChild(resetButton);
+    resetButton.addEventListener('click', ()=>{
+        gameplayScreen.removeChild(resetButton);
+        restart();
+    });
 }
 
 
 function restart(){
     startScreen.style.display = 'none';
-    gameoverScreen.style.display = 'none';
     gameplayScreen.style.display = 'flex';
+
+    let question = random_perm(N*N);
+    grid.innerHTML='';
+    memo.textContent = '00.0';
+    execute.textContent = '00.0';
+    stopwatch.textContent = '00.0';
+    const isPortrait = window.matchMedia("(orientation: portrait)").matches;
+    const columnWidth = isPortrait ? `${Math.floor(90/N)}vw` : `${Math.floor(70/N)}vh`;
+    grid.style.gridTemplateColumns = `repeat(${N}, ${columnWidth})`;
+
+    for (let i = 0; i<N*N; i++){
+        const newCell = document.createElement('button');
+        newCell.classList.add('cell');
+        const textsize = isPortrait ? `${Math.floor(50/N)}vw` : `${Math.floor(50/N)}vh`;
+
+        newCell.style.fontSize = textsize;
+        newCell.textContent = question[i];
+        grid.appendChild(newCell);
+    }
+   
     const cells = document.querySelectorAll('.cell');
+    let execute_trigger = false;
     cells.forEach(cell =>{
         cell.addEventListener('click', ()=>{
             cell.classList.add('chosen');
             chosen.push(cell);
+            clearInterval(intervalMemo);
+            if (!execute_trigger){
+                intervalExecute = setInterval(updateExecute, 100);
+                execute_trigger = true;
+                cells.forEach(cell =>{cell.style.textIndent = '-9999px'; })
+            }
         })
     })
+    intervalStopwatch = setInterval(updateStopwatch, 100);
+    intervalMemo = setInterval(updateMemo, 100);
 }
 
 
+
+function updateMemo(){
+    memo.textContent = (Number(memo.textContent)+0.1).toFixed(1).padStart(4, '0');
+}
+function updateExecute(){
+    execute.textContent = (Number(execute.textContent)+0.1).toFixed(1).padStart(4, '0');
+}
+function updateStopwatch(){
+    stopwatch.textContent = (Number(stopwatch.textContent)+0.1).toFixed(1).padStart(4, '0');
+}
 
 
 
@@ -84,7 +134,6 @@ restartButtons.forEach(button =>{
 
 
 
-window.requestAnimationFrame(update)
 
 
 //Utility
